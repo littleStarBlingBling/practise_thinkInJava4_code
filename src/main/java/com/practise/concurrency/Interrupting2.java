2cd418b023a7a3606a6cdf8297c77cfac57034b6
littleStarBlingBling <2178728041@qq.com>
2021-03-12 21:40:40 +0800

    [add] 12/3/2020

--- src/main/java/com/practise/concurrency/Interrupting2.java
+++ src/main/java/com/practise/concurrency/Interrupting2.java
@@ -1,4 +1,45 @@
 package com.practise.concurrency;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class BlockedMutex{
+    private Lock lock = new ReentrantLock();
+
+    // 在构造器里获取锁，并且不释放
+    public BlockedMutex() {
+        lock.lock();
+    }
+
+    public void f(){
+        try{
+            // 可以中断锁的获取
+            lock.lockInterruptibly();
+            System.out.println("lock acquired in f()");
+        }catch (InterruptedException e){
+            System.out.println("Interrupted from lock acquisition in f()");
+        }
+    }
+}
+
+class Blocked2 implements Runnable{
+    BlockedMutex blockedMutex = new BlockedMutex();
+
+    @Override
+    public void run() {
+        System.out.println("Waiting for f() in BlockedMutex");
+        blockedMutex.f();
+        System.out.println("Broken out of blocked call");
+    }
+}
+
 public class Interrupting2 {
+    public static void main(String[] args) throws Exception{
+        Thread thread = new Thread(new Blocked2());
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Issuing t.interrupt()");
+        thread.interrupt();
+    }
 }
--- src/main/java/com/practise/concurrency/InterruptingIdiom.java
+++ src/main/java/com/practise/concurrency/InterruptingIdiom.java
@@ -1,4 +1,61 @@
 package com.practise.concurrency;
 
+import java.util.concurrent.TimeUnit;
+
+class NeedsCleanup{
+    private final int id;
+
+    public NeedsCleanup(int id) {
+        this.id = id;
+        System.out.println("NeedsCleanup " + id);
+    }
+
+    public void cleanup(){
+        System.out.println("Cleaning up " + id);
+    }
+}
+
+class Blocked3 implements Runnable{
+    private volatile double d = 0.0;
+
+    @Override
+    public void run() {
+        try{
+            while (!Thread.interrupted()){
+                // point1
+                NeedsCleanup n1 = new NeedsCleanup(1);
+                try{
+                    System.out.println("Sleeping");
+                    TimeUnit.SECONDS.sleep(1);
+
+                    // point2
+                    NeedsCleanup n2 = new NeedsCleanup(2);
+                    try{
+                        System.out.println("Calculating");
+                        for(int i = 1; i< 2500000; i++){
+                            d = d + (Math.PI + Math.E) / d;
+                        }
+
+                        System.out.println("Finished time-consuming operation");
+                    }finally {
+                        n2.cleanup();
+                    }
+                }finally {
+                    n1.cleanup();
+                }
+            }
+            System.out.println("Exiting via while() test");
+        }catch (InterruptedException e){
+            System.out.println("Exiting via InterruptedException");
+        }
+    }
+}
+
 public class InterruptingIdiom {
+    public static void main(String[] args) throws Exception{
+       Thread thread = new Thread(new Blocked3());
+       thread.start();
+       TimeUnit.MILLISECONDS.sleep(990);
+       thread.interrupt();
+    }
 }
